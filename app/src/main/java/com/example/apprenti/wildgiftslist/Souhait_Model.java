6221f86d055f8387ed41d91a5dde2929fbec7465
Sautronl <sautronl31@gmail.com>
2017-10-30 11:43:54 +0100

    modelsouhait

--- app/src/main/java/com/example/apprenti/wildgiftslist/Souhait_Model.java
+++ app/src/main/java/com/example/apprenti/wildgiftslist/Souhait_Model.java
@@ -5,31 +5,52 @@ package com.example.apprenti.wildgiftslist;
  */
 
 public class Souhait_Model {
+    private String name;
     private String desc;
-    private String Url;
+    private String urlImg;
+    private String url;
 
     private Souhait_Model(){
 
     }
 
-    public Souhait_Model(String desc, String url) {
+    public Souhait_Model(String name, String desc, String urlImg,String url) {
+        this.name = name;
         this.desc = desc;
-        Url = url;
+        this.urlImg = urlImg;
+        this.url = url;
     }
 
     public String getDesc() {
-        return desc;
+        return name;
     }
 
     public void setDesc(String desc) {
-        this.desc = desc;
+        this.name = desc;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
     }
 
     public String getUrl() {
-        return Url;
+        return url;
     }
 
     public void setUrl(String url) {
-        Url = url;
+        this.url = url;
     }
 }
