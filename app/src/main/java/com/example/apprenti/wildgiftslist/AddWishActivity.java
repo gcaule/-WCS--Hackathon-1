264d33daffb7b3c45bcee1ca188aafccb08f4b41
Germain Caule <g.caule31@outlook.fr>
2017-10-30 11:42:21 +0100

    Design add wish activity

---
+++ app/src/main/java/com/example/apprenti/wildgiftslist/AddWishActivity.java
@@ -0,0 +1,65 @@
+package com.example.apprenti.wildgiftslist;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddWishActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFire;
+    private DatabaseReference mDbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wish);
+
+        mFire = FirebaseDatabase.getInstance();
+        mDbRef = mFire.getReference("user/wish");
+
+        getSupportActionBar().setTitle(R.string.add_wish);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button abort = findViewById(R.id.abort);
+        abort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button create = findViewById(R.id.button_create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+}
--- app/src/main/java/com/example/apprenti/wildgiftslist/MainActivity.java
+++ app/src/main/java/com/example/apprenti/wildgiftslist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.apprenti.wildgiftslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testButton = findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent test = new Intent(MainActivity.this, AddWishActivity.class);
+                startActivity(test);
+            }
+        });
+
     }
 }
